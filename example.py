963e4d9cde61a5ae4ebff8bfad0618ae60e1730e
jrivers33 <jrivers@trend9-marketing.com>
2025-05-04 03:46:08 +0000

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Creating array for number scopes
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
